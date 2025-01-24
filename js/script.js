b0af2b6e2a29b6961208146e80dbdaaf5302cdb9
fernando070274 <fernando.pereira640@hotmail.com>
2025-01-24 12:17:35 -0500

    resumepg-part01

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $(function(){
         $("#header").load("./header.html")
         $("#footer").load("./footer.html")
+        $("#footer-resume").load("./footer-resume.html")
     });
 });
 
