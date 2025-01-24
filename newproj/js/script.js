bb1bfadc29aaf89471930aee6c5f496ef47782db
fernando070274 <fernando.pereira640@hotmail.com>
2025-01-23 19:29:00 -0500

    New wb

---
+++ newproj/js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $(function(){
+        $("#header").load("./header.html")
+        $("#footer").load("./footer.html")
+    });
+});
+
+function slideRight(){
+    var reveals = document.querySelectorAll(".slide-right");
+    for(var i = 0; i < reveals.length; i++){
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 100;
+        if(elementTop < windowHeight - elementVisible){
+            reveals[i].classList.add("active");
+        }else{
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+window.addEventListener("scroll", slideRight);
+
+function slideLeft(){
+    var reveals = document.querySelectorAll(".slide-left");
+    for(var i = 0; i < reveals.length; i++){
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 100;
+        if(elementTop < windowHeight - elementVisible){
+            reveals[i].classList.add("active");
+        }else{
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+window.addEventListener("scroll", slideLeft);
